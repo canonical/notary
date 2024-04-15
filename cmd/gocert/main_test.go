a694213ef20a6b31746f415cf1739b44669f5331
Kayra <kayra.gemalmaz@canonical.com>
2024-04-15 16:39:23 +0300

    use consts

--- cmd/gocert/main_test.go
+++ cmd/gocert/main_test.go
@@ -11,8 +11,15 @@ const (
 certpath: "./cert_test.pem"
 dbpath: "./certs.db"
 port: 8000`
-	invalidYAMLConfig = `wrong: fields
-every: where`
+	invalidCertConfig = `keypath:  "./key_test.pem"
+certpath: "./wrong_cert_test.pem"
+dbpath: "./certs.db"
+port: 8000`
+
+	invalidDBConfig = `keypath:  "./nokeyfile.pem"
+certpath: "./nocertfile.pem"
+dbpath: "/etc/hosts"
+port: 8000`
 	invalidFileConfig = `keypath:  "./nokeyfile.pem"
 certpath: "./nocertfile.pem"
 dbpath: "./certs.db"
@@ -25,13 +32,14 @@ func TestGoCertFail(t *testing.T) {
 	cases := []struct {
 		Name           string
 		Args           []string
+		ConfigYAML     string
 		ExpectedExit   int
 		ExpectedOutput string
 	}{
-		{"flags not set", []string{}, 1, "Providing a valid config file is required."},
-		{"config file not valid", []string{"-config", "config_invalid.yaml"}, 1, "Config file validation failed:"},
-		{"database not connectable", []string{"-config", "config_invalid_db.yaml"}, 1, "Couldn't connect to database:"},
-		{"server couldn't be created", []string{"-config", "config_invalid.yaml"}, 1, "Couldn't create server:"},
+		{"flags not set", []string{}, validConfig, 1, "Providing a valid config file is required."},
+		{"config file not valid", []string{"-config", "config_invalid.yaml"}, invalidFileConfig, 1, "Config file validation failed:"},
+		{"database not connectable", []string{"-config", "config_invalid_db.yaml"}, invalidDBConfig, 1, "Couldn't connect to database:"},
+		{"server couldn't be created", []string{"-config", "config_invalid.yaml"}, invalidCertConfig, 1, "Couldn't create server:"},
 	}
 	for _, tc := range cases {
 		flag.CommandLine = flag.NewFlagSet(tc.Name, flag.ExitOnError)

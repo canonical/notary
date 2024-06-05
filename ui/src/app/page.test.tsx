df6e29f135961baa67f7fb185c34d46930ca7777
kayra1 <gemalmazkayra@gmail.com>
2024-06-05 18:43:33 +0300

    remove homepage completely

--- ui/src/app/page.test.tsx
+++
@@ -1,8 +0,0 @@
-import { expect, test } from 'vitest'
-import { render, screen } from '@testing-library/react'
-import Page from './page'
-
-test('HomePage', () => {
-    render(<Page />)
-    expect(screen.getByText(/Welcome to GoCert/i)).toBeDefined()
-})
--- ui/src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Main() {
-  return (
-    <div>Welcome to GoCert</div>
-  );
-}

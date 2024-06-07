21428196d8de228e50d012fee82c13aae7709405
Kayra <kayra.gemalmaz@canonical.com>
2024-06-07 11:08:14 +0300

    feat: Pebble Notices (#20)
    Co-authored-by: Ghislain Bourgeois <ghislain.bourgeois@gmail.com>
    Co-authored-by: Guillaume Belanger <guillaume.belanger@canonical.com>

--- README.md
+++ README.md
@@ -15,11 +15,30 @@ GoCert requires 3 files to operate:
 * A private key
 * A TLS certificate with that private key
 * A YAML config file with the required parameters
-as an example:
+
+You can generate the cert and the associated key by running:
+```
+openssl req -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 1 -out cert.pem -subj "/CN=example.com"
+```
+
+GoCert does not support insecure http connections.
+
+### Config File
+The config file requires the following parameters:
+| Key                  | Type    | Description |
+|----------------------|---------|----------|
+| key_path             | string  | path to the private key for enabling HTTPS connections |
+| cert_path            | string  | path to a PEM formatted certificate for enabling HTTPS connections |
+| db_path              | string  | path to a sqlite database file. If the file does not exist GoCert will attempt to create it. |
+| port                 | integer (0-65535)  | port number on which GoCert will listen for all incoming API and frontend connections. |
+| pebble_notifications | boolean | Allow GoCert to send pebble notices on certificate events (create, update, delete). Pebble needs to be running on the same system as GoCert. Read more about Pebble Notices [here](https://github.com/canonical/pebble?tab=readme-ov-file#notices). |
+
+An example config file may look like:
 
 ```yaml
-keypath:  "./key.pem"
-certpath: "./cert.pem"
-dbpath: "./certs.db"
+key_path:  "./key.pem"
+cert_path: "./cert.pem"
+db_path: "./certs.db"
 port: 3000
+pebble_notifications: true
 ```
--- internal/api/server.go
+++ internal/api/server.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"time"
 
 	"github.com/canonical/gocert/internal/certdb"
@@ -15,21 +16,24 @@ import (
 )
 
 type ConfigYAML struct {
-	KeyPath  string
-	CertPath string
-	DBPath   string
-	Port     int
+	KeyPath                    string `yaml:"key_path"`
+	CertPath                   string `yaml:"cert_path"`
+	DBPath                     string `yaml:"db_path"`
+	Port                       int    `yaml:"port"`
+	Pebblenotificationsenabled bool   `yaml:"pebble_notifications"`
 }
 
 type Config struct {
-	Key    []byte
-	Cert   []byte
-	DBPath string
-	Port   int
+	Key                        []byte
+	Cert                       []byte
+	DBPath                     string
+	Port                       int
+	PebbleNotificationsEnabled bool
 }
 
 type Environment struct {
-	DB *certdb.CertificateRequestsRepository
+	DB                      *certdb.CertificateRequestsRepository
+	SendPebbleNotifications bool
 }
 
 // validateConfigFile opens and processes the given yaml file, and catches errors in the process
@@ -65,7 +69,16 @@ func validateConfigFile(filePath string) (Config, error) {
 	config.Key = key
 	config.DBPath = c.DBPath
 	config.Port = c.Port
+	config.PebbleNotificationsEnabled = c.Pebblenotificationsenabled
 	return config, nil
+}
+
+func SendPebbleNotification(key, request_id string) error {
+	cmd := exec.Command("pebble", "notify", key, fmt.Sprintf("request_id=%s", request_id))
+	if err := cmd.Run(); err != nil {
+		return errors.Join(errors.New("couldn't execute a pebble notify: "), err)
+	}
+	return nil
 }
 
 // NewServer creates an environment and an http server with handlers that Go can start listening to
@@ -85,6 +98,7 @@ func NewServer(configFile string) (*http.Server, error) {
 
 	env := &Environment{}
 	env.DB = db
+	env.SendPebbleNotifications = config.PebbleNotificationsEnabled
 	router := NewGoCertRouter(env)
 
 	s := &http.Server{

6cd64bf9d41f5b58691f854654abd3de26387c80
kayra1 <gemalmazkayra@gmail.com>
2024-06-21 16:28:06 +0300

    add modals

---
+++ ui/src/app/certificate_requests/modals.tsx
@@ -0,0 +1,29 @@
+import { Dispatch, SetStateAction } from "react"
+import { ConfirmationModalData } from "./row"
+
+interface ConfirmationModalProps {
+    modalData: ConfirmationModalData
+    setModalData: Dispatch<SetStateAction<ConfirmationModalData>>
+}
+
+
+export function ConfirmationModal({ modalData, setModalData }: ConfirmationModalProps) {
+    const confirmQuery = () => {
+        modalData?.func()
+        setModalData(null)
+    }
+    return (
+        <div className="p-modal" id="modal">
+            <section className="p-modal__dialog" role="dialog" aria-modal="true" aria-labelledby="modal-title" aria-describedby="modal-description">
+                <header className="p-modal__header">
+                    <h2 className="p-modal__title" id="modal-title">Confirm Action</h2>
+                </header>
+                <p>{modalData?.warningText}</p>
+                <footer className="p-modal__footer">
+                    <button className="u-no-margin--bottom" aria-controls="modal" onMouseDown={() => setModalData(null)}>Cancel</button>
+                    <button className="p-button--negative u-no-margin--bottom" onMouseDown={confirmQuery}>Confirm</button>
+                </footer>
+            </section>
+        </div>
+    )
+}
--- ui/src/app/certificate_requests/row.tsx
+++ ui/src/app/certificate_requests/row.tsx
@@ -1,8 +1,8 @@
 import { useState, Dispatch, SetStateAction, useEffect, useRef } from "react"
-import { useMutation, useQueryClient } from "react-query"
+import { UseMutationResult, useMutation, useQueryClient } from "react-query"
 import { extractCSR, extractCert } from "../utils"
 import { deleteCSR, rejectCSR } from "../queries"
-
+import { ConfirmationModal } from "./modals"
 
 type rowProps = {
     id: number,
@@ -12,8 +12,14 @@ type rowProps = {
     ActionMenuExpanded: number
     setActionMenuExpanded: Dispatch<SetStateAction<number>>
 }
+export type ConfirmationModalData = {
+    func: () => void
+    warningText: string
+} | null
+
 export default function Row({ id, csr, certificate, ActionMenuExpanded, setActionMenuExpanded }: rowProps) {
     const [detailsMenuOpen, setDetailsMenuOpen] = useState<boolean>(false)
+    const [confirmationModalData, setConfirmationModalData] = useState<ConfirmationModalData>(null)
 
     const csrObj = extractCSR(csr)
     const certObj = extractCert(certificate)
@@ -26,6 +32,22 @@ export default function Row({ id, csr, certificate, ActionMenuExpanded, setActio
         onSuccess: () => queryClient.invalidateQueries('csrs')
     })
 
+    const mutationFunc = (mutation: UseMutationResult<any, unknown, string, unknown>) => {
+        mutation.mutate(id.toString())
+    }
+    const handleReject = () => {
+        setConfirmationModalData({
+            func: () => () => mutationFunc(rejectMutation),
+            warningText: "Are you sure you want to reject this CSR? This action cannot be undone and will remove the certificate if there is any."
+        })
+    }
+    const handleDelete = () => {
+        setConfirmationModalData({
+            func: () => () => mutationFunc(deleteMutation),
+            warningText: "Are you sure you want to delete this CSR? This action cannot be undone and will delete the certificate if there is any."
+        })
+    }
+
     const toggleActionMenu = () => {
         if (ActionMenuExpanded == id) {
             setActionMenuExpanded(0)
@@ -34,53 +56,56 @@ export default function Row({ id, csr, certificate, ActionMenuExpanded, setActio
         }
     }
     return (
-        <tr>
-            <td className="" width={5} aria-label="id">{id}</td>
-            <td className="">
-                <button
-                    className="u-toggle p-contextual-menu__toggle p-button--base is-small"
-                    aria-controls="expanded-row"
-                    aria-expanded={detailsMenuOpen ? "true" : "false"}
-                    onClick={() => setDetailsMenuOpen(!detailsMenuOpen)}>
-                    <i className="p-icon--chevron-down p-contextual-menu__indicator"></i>
-                </button>
-                <span>{csrObj.subjects.find((e) => e.type == "Common Name")?.value}</span>
-            </td>
-            <td className="" aria-label="csr-status">{certificate == "" ? "outstanding" : (certificate == "rejected" ? "rejected" : "fulfilled")}</td>
-            <td className="" aria-label="certificate-expiry-date">{certificate == "" ? "" : (certificate == "rejected" ? "" : certObj?.notAfter)}</td>
-            <td className="has-overflow" data-heading="Actions">
-                <span className="p-contextual-menu--center u-no-margin--bottom">
+        <>
+            <tr>
+                <td className="" width={5} aria-label="id">{id}</td>
+                <td className="">
                     <button
-                        className="p-contextual-menu__toggle p-button--base is-small u-no-margin--bottom"
-                        aria-label="action-menu-button"
-                        aria-controls="action-menu"
-                        aria-expanded={ActionMenuExpanded == id ? "true" : "false"}
-                        aria-haspopup="true"
-                        onClick={toggleActionMenu}
-                        onBlur={toggleActionMenu}>
-                        <i className="p-icon--menu p-contextual-menu__indicator"></i>
+                        className="u-toggle p-contextual-menu__toggle p-button--base is-small"
+                        aria-controls="expanded-row"
+                        aria-expanded={detailsMenuOpen ? "true" : "false"}
+                        onClick={() => setDetailsMenuOpen(!detailsMenuOpen)}>
+                        <i className="p-icon--chevron-down p-contextual-menu__indicator"></i>
                     </button>
-                    <span className="p-contextual-menu__dropdown" id="action-menu" aria-hidden={ActionMenuExpanded == id ? "false" : "true"}>
-                        <span className="p-contextual-menu__group">
-                            <button className="p-contextual-menu__link">Copy Certificate Request to Clipboard</button>
-                            <button className="p-contextual-menu__link">Download Certificate Request</button>
-                            <button className="p-contextual-menu__link" onMouseDown={() => rejectMutation.mutate(id.toString())}>Reject Certificate Request</button>
-                            <button className="p-contextual-menu__link" onMouseDown={() => deleteMutation.mutate(id.toString())}>Delete Certificate Request</button>
-                        </span>
-                        <span className="p-contextual-menu__group">
-                            <button className="p-contextual-menu__link">Upload Certificate</button>
-                            <button className="p-contextual-menu__link">Revoke Certificate</button>
+                    <span>{csrObj.subjects.find((e) => e.type == "Common Name")?.value}</span>
+                </td>
+                <td className="" aria-label="csr-status">{certificate == "" ? "outstanding" : (certificate == "rejected" ? "rejected" : "fulfilled")}</td>
+                <td className="" aria-label="certificate-expiry-date">{certificate == "" ? "" : (certificate == "rejected" ? "" : certObj?.notAfter)}</td>
+                <td className="has-overflow" data-heading="Actions">
+                    <span className="p-contextual-menu--center u-no-margin--bottom">
+                        <button
+                            className="p-contextual-menu__toggle p-button--base is-small u-no-margin--bottom"
+                            aria-label="action-menu-button"
+                            aria-controls="action-menu"
+                            aria-expanded={ActionMenuExpanded == id ? "true" : "false"}
+                            aria-haspopup="true"
+                            onClick={toggleActionMenu}
+                            onBlur={toggleActionMenu}>
+                            <i className="p-icon--menu p-contextual-menu__indicator"></i>
+                        </button>
+                        <span className="p-contextual-menu__dropdown" id="action-menu" aria-hidden={ActionMenuExpanded == id ? "false" : "true"}>
+                            <span className="p-contextual-menu__group">
+                                <button className="p-contextual-menu__link">Copy Certificate Request to Clipboard</button>
+                                <button className="p-contextual-menu__link">Download Certificate Request</button>
+                                <button className="p-contextual-menu__link" onMouseDown={handleReject}>Reject Certificate Request</button>
+                                <button className="p-contextual-menu__link" onMouseDown={handleDelete}>Delete Certificate Request</button>
+                            </span>
+                            <span className="p-contextual-menu__group">
+                                <button className="p-contextual-menu__link">Upload Certificate</button>
+                                <button className="p-contextual-menu__link">Revoke Certificate</button>
+                            </span>
                         </span>
                     </span>
-                </span>
-            </td>
-            <td id="expanded-row" className="p-table__expanding-panel" aria-hidden={detailsMenuOpen ? "false" : "true"}>
-                <div className="row">
-                    <div className="col-8">
-                        <p><b>Common Name</b>: {csrObj.subjects.find((e) => e.type == "Common Name")?.value}</p>
+                </td>
+                <td id="expanded-row" className="p-table__expanding-panel" aria-hidden={detailsMenuOpen ? "false" : "true"}>
+                    <div className="row">
+                        <div className="col-8">
+                            <p><b>Common Name</b>: {csrObj.subjects.find((e) => e.type == "Common Name")?.value}</p>
+                        </div>
                     </div>
-                </div>
-            </td>
-        </tr>
+                </td>
+            </tr>
+            {confirmationModalData != null && <ConfirmationModal modalData={confirmationModalData} setModalData={setConfirmationModalData} />}
+        </>
     )
 }

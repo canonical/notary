412bb63dbe9f8176d7125de3c3196e180a5e3e90
saltiyazan <yazan.salti.19@gmail.com>
2024-07-05 10:15:22 +0200

    feat: Adds handlers for DELETE for user accounts (#38)

--- internal/api/handlers_test.go
+++ internal/api/handlers_test.go
@@ -441,6 +441,22 @@ func TestGoCertUsersHandlers(t *testing.T) {
 			response: "error: Username is required",
 			status:   http.StatusBadRequest,
 		},
+		{
+			desc:     "Delete user success",
+			method:   "DELETE",
+			path:     "/api/v1/accounts/2",
+			data:     invalidUser,
+			response: "1",
+			status:   http.StatusAccepted,
+		},
+		{
+			desc:     "Delete user failure",
+			method:   "DELETE",
+			path:     "/api/v1/accounts/2",
+			data:     invalidUser,
+			response: "error: id not found",
+			status:   http.StatusNotFound,
+		},
 	}
 	for _, tC := range testCases {
 		t.Run(tC.desc, func(t *testing.T) {

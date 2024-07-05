d2a9309b35f9ee9a96e426c2da6be2ef1da3baac
yazansalti <yazan.salti@canonical.com>
2024-07-05 09:02:22 +0200

    Fixes url

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -34,7 +34,7 @@ func NewGoCertRouter(env *Environment) http.Handler {
 	apiV1Router.HandleFunc("GET /accounts/{id}", GetUserAccount(env))
 	apiV1Router.HandleFunc("GET /accounts", GetUserAccounts(env))
 	apiV1Router.HandleFunc("POST /accounts", PostUserAccount(env))
-	apiV1Router.HandleFunc("DELETE /accounts/{id}/account", DeleteUserAccount(env))
+	apiV1Router.HandleFunc("DELETE /accounts/{id}", DeleteUserAccount(env))
 
 	m := metrics.NewMetricsSubsystem(env.DB)
 	frontendHandler := newFrontendFileServer()

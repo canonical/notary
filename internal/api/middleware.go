1b753cc069347125db8944b025ce46cf1dc31361
kayra1 <gemalmazkayra@gmail.com>
2024-07-10 17:25:53 +0300

    allow all paths outside of the API to be open

--- internal/api/middleware.go
+++ internal/api/middleware.go
@@ -103,7 +103,7 @@ func authMiddleware(ctx *middlewareContext) middleware {
 
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if strings.HasPrefix(r.URL.Path, "/metrics") || strings.HasPrefix(r.URL.Path, "/status") || strings.HasPrefix(r.URL.Path, "/login") {
+			if !strings.HasPrefix(r.URL.Path, "/api/v1/") {
 				next.ServeHTTP(w, r)
 				return
 			}

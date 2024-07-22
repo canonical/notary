823576ae53da7b5ae032d2761a1283ba2f38e761
kayra1 <gemalmazkayra@gmail.com>
2024-07-22 15:01:21 +0300

    login page

--- ui/src/app/login/page.tsx
+++ ui/src/app/login/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import { useMutation } from "react-query"
 import { login } from "../queries"
+import { useMutation } from "react-query"
 import { useState, ChangeEvent } from "react"
 import { useCookies } from "react-cookie"
 import { useRouter } from "next/navigation"
@@ -30,29 +30,35 @@ export default function LoginPage() {
     const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) => { setUsername(event.target.value) }
     const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => { setPassword(event.target.value) }
     return (
-        <div className="p-panel">
-            <div className="p-panel__header is-sticky">
-                <h4 className="p-panel__title">Login</h4>
-            </div>
-            <div className="p-panel__content">
-                <div className="u-fixed-width">
-                    <form>
-                        <label htmlFor="InputUsername">User Name</label>
-                        <input type="text" id="InputUsername" name="InputUsername" onChange={handleUsernameChange} />
-                        <label htmlFor="InputPassword">Password</label>
-                        <input type="password" id="InputPassword" name="InputPassword" placeholder="******" autoComplete="current-password" onChange={handlePasswordChange} />
-                        <button type="submit" name="submit" onClick={(event) => { event.preventDefault(); mutation.mutate({ username: username, password: password }) }}>Submit</button>
-                        {errorText &&
-                            <div className="p-notification--negative">
-                                <div className="p-notification__content">
-                                    <h5 className="p-notification__title">Error Logging In</h5>
-                                    <p className="p-notification__message">{errorText.split("error: ")}</p>
-                                </div>
-                            </div>
-                        }
-                    </form>
+        <div>
+            <div className="p-panel" style={{
+                width: "25rem",
+                minWidth: "min-content",
+                margin: "auto"
+            }}>
+                <div className="p-panel__header is-sticky">
+                    <h4 className="p-panel__title">Login</h4>
                 </div>
-            </div>
+                <div className="p-panel__content">
+                    <div className="u-fixed-width">
+                        <form>
+                            <label htmlFor="InputUsername">User Name</label>
+                            <input type="text" id="InputUsername" name="InputUsername" onChange={handleUsernameChange} />
+                            <label htmlFor="InputPassword">Password</label>
+                            <input type="password" id="InputPassword" name="InputPassword" placeholder="******" autoComplete="current-password" onChange={handlePasswordChange} />
+                            {errorText &&
+                                <div className="p-notification--negative">
+                                    <div className="p-notification__content">
+                                        <h5 className="p-notification__title">Error Logging In</h5>
+                                        <p className="p-notification__message">{errorText.split("error: ")}</p>
+                                    </div>
+                                </div>
+                            }
+                            <button type="submit" name="submit" onClick={(event) => { event.preventDefault(); mutation.mutate({ username: username, password: password }) }}>Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div >
         </div >
     )
 }

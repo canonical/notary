89da8705514174055655f19c62f5ff8eff0a3099
kayra1 <gemalmazkayra@gmail.com>
2024-07-22 13:30:12 +0300

    logged in button ui

--- ui/src/app/login.tsx
+++ ui/src/app/login.tsx
@@ -6,12 +6,14 @@ export function AccountTab() {
         <>
             {
                 authDetails.user ?
-                    <div className="p-side-navigation__item" aria-current="false">
+                    <div className="p-side-navigation__link" aria-current="false">
                         <i className="p-icon--user is-light p-side-navigation__icon"></i>
                         <span className="p-side-navigation__label">
                             <span className="p-side-navigation__label">{authDetails.user.username}</span>
                         </span>
-                        <i className="p-icon--menu" style={{ marginLeft: "auto", marginTop: "auto", marginBottom: "auto" }}></i>
+                        <div className="p-side-navigation__status">
+                            <i className="p-icon--menu"></i>
+                        </div>
                     </div>
                     :
                     <a className="p-side-navigation__link" href="/login" aria-current="false">

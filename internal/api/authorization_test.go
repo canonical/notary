b45072575381adfe0577eaf69cea638fa364c30a
Guillaume Belanger <guillaume.belanger@canonical.com>
2024-09-10 11:30:24 -0400

    chore: separate http handlers in smaller files (#75)
    Signed-off-by: guillaume <guillaume.belanger27@gmail.com>

---
+++ internal/api/authorization_test.go
@@ -0,0 +1,168 @@
+package server_test
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"regexp"
+	"strings"
+	"testing"
+
+	server "github.com/canonical/notary/internal/api"
+	"github.com/canonical/notary/internal/db"
+)
+
+func TestAuthorization(t *testing.T) {
+	testdb, err := db.NewDatabase(":memory:")
+	if err != nil {
+		log.Fatalf("couldn't create test sqlite db: %s", err)
+	}
+	env := &server.Environment{}
+	env.DB = testdb
+	env.JWTSecret = []byte("secret")
+	ts := httptest.NewTLSServer(server.NewNotaryRouter(env))
+	defer ts.Close()
+
+	client := ts.Client()
+	var adminToken string
+	var nonAdminToken string
+	t.Run("prepare user accounts and tokens", prepareUserAccounts(ts.URL, client, &adminToken, &nonAdminToken))
+
+	testCases := []struct {
+		desc     string
+		method   string
+		path     string
+		data     string
+		auth     string
+		response string
+		status   int
+	}{
+		{
+			desc:     "metrics reachable without auth",
+			method:   "GET",
+			path:     "/metrics",
+			data:     "",
+			auth:     "",
+			response: "# HELP certificate_requests Total number of certificate requests",
+			status:   http.StatusOK,
+		},
+		{
+			desc:     "status reachable without auth",
+			method:   "GET",
+			path:     "/status",
+			data:     "",
+			auth:     "",
+			response: "",
+			status:   http.StatusOK,
+		},
+		{
+			desc:     "missing endpoints produce 404",
+			method:   "GET",
+			path:     "/this/path/does/not/exist",
+			data:     "",
+			auth:     nonAdminToken,
+			response: "",
+			status:   http.StatusNotFound,
+		},
+		{
+			desc:     "nonadmin can't see accounts",
+			method:   "GET",
+			path:     "/api/v1/accounts",
+			data:     "",
+			auth:     nonAdminToken,
+			response: "",
+			status:   http.StatusForbidden,
+		},
+		{
+			desc:     "admin can see accounts",
+			method:   "GET",
+			path:     "/api/v1/accounts",
+			data:     "",
+			auth:     adminToken,
+			response: `[{"id":1,"username":"testadmin","permissions":1},{"id":2,"username":"testuser","permissions":0}]`,
+			status:   http.StatusOK,
+		},
+		{
+			desc:     "nonadmin can't delete admin account",
+			method:   "DELETE",
+			path:     "/api/v1/accounts/1",
+			data:     "",
+			auth:     nonAdminToken,
+			response: "",
+			status:   http.StatusForbidden,
+		},
+		{
+			desc:     "user can't change admin password",
+			method:   "POST",
+			path:     "/api/v1/accounts/1/change_password",
+			data:     `{"password":"Pwnd123!"}`,
+			auth:     nonAdminToken,
+			response: "",
+			status:   http.StatusForbidden,
+		},
+		{
+			desc:     "user can change self password with /me",
+			method:   "POST",
+			path:     "/api/v1/accounts/me/change_password",
+			data:     `{"password":"BetterPW1!"}`,
+			auth:     nonAdminToken,
+			response: "",
+			status:   http.StatusOK,
+		},
+		{
+			desc:     "user can login with new password",
+			method:   "POST",
+			path:     "/login",
+			data:     `{"username":"testuser","password":"BetterPW1!"}`,
+			auth:     nonAdminToken,
+			response: "",
+			status:   http.StatusOK,
+		},
+		{
+			desc:     "admin can't delete itself",
+			method:   "DELETE",
+			path:     "/api/v1/accounts/1",
+			data:     "",
+			auth:     adminToken,
+			response: "error: deleting an Admin account is not allowed.",
+			status:   http.StatusBadRequest,
+		},
+		{
+			desc:     "admin can delete nonuser",
+			method:   "DELETE",
+			path:     "/api/v1/accounts/2",
+			data:     "",
+			auth:     adminToken,
+			response: "1",
+			status:   http.StatusAccepted,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			req, err := http.NewRequest(tC.method, ts.URL+tC.path, strings.NewReader(tC.data))
+			req.Header.Add("Authorization", "Bearer "+tC.auth)
+			if err != nil {
+				t.Fatal(err)
+			}
+			res, err := client.Do(req)
+			if err != nil {
+				t.Fatal(err)
+			}
+			resBody, err := io.ReadAll(res.Body)
+			res.Body.Close()
+			if err != nil {
+				t.Fatal(err)
+			}
+			if res.StatusCode != tC.status || !strings.Contains(string(resBody), tC.response) {
+				t.Errorf("expected response did not match.\nExpected vs Received status code: %d vs %d\nExpected vs Received body: \n%s\nvs\n%s\n", tC.status, res.StatusCode, tC.response, string(resBody))
+			}
+			if tC.desc == "Create no password user success" {
+				match, _ := regexp.MatchString(`"password":"[!-~]{16}"`, string(resBody))
+				if !match {
+					t.Errorf("password does not match expected format or length: got %s", string(resBody))
+				}
+			}
+		})
+	}
+}
---
+++ internal/api/handlers_certificate_requests.go
@@ -0,0 +1,195 @@
+package server
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/canonical/notary/internal/db"
+)
+
+// GetCertificateRequests returns all of the Certificate Requests
+func GetCertificateRequests(env *Environment) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		certs, err := env.DB.RetrieveAllCSRs()
+		if err != nil {
+			logErrorAndWriteResponse(err.Error(), http.StatusInternalServerError, w)
+			return
+		}
+		body, err := json.Marshal(certs)
+		if err != nil {
+			logErrorAndWriteResponse(err.Error(), http.StatusInternalServerError, w)
+			return
+		}
+		if _, err := w.Write(body); err != nil {
+			logErrorAndWriteResponse(err.Error(), http.StatusInternalServerError, w)
+		}
+	}
+}
+
+// PostCertificateRequest creates a new Certificate Request, and returns the id of the created row
+func PostCertificateRequest(env *Environment) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		csr, err := io.ReadAll(r.Body)
+		if err != nil {
+			logErrorAndWriteResponse(err.Error(), http.StatusInternalServerError, w)
+			return
+		}
+		id, err := env.DB.CreateCSR(string(csr))
+		if err != nil {
+			if strings.Contains(err.Error(), "UNIQUE constraint failed") {
+				logErrorAndWriteResponse("given csr already recorded", http.StatusBadRequest, w)
+				return
+			}
+			if strings.Contains(err.Error(), "csr validation failed") {
+				logErrorAndWriteResponse(err.Error(), http.StatusBadRequest, w)
+				return
+			}
+			logErrorAndWriteResponse(err.Error(), http.StatusInternalServerError, w)
+			return
+		}
+		w.WriteHeader(http.StatusCreated)
+		if _, err := w.Write([]byte(strconv.FormatInt(id, 10))); err != nil {
+			logErrorAndWriteResponse(err.Error(), http.StatusInternalServerError, w)
+		}
+	}
+}
+
+// GetCertificateRequests receives an id as a path parameter, and
+// returns the corresponding Certificate Request
+func GetCertificateRequest(env *Environment) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		cert, err := env.DB.RetrieveCSR(id)
+		if err != nil {
+			if errors.Is(err, db.ErrIdNotFound) {
+				logErrorAndWriteResponse(err.Error(), http.StatusNotFound, w)
+				return
+			}
+			logErrorAndWriteResponse(err.Error(), http.StatusInternalServerError, w)
+			return
+		}
+		body, err := json.Marshal(cert)
+		if err != nil {
+			logErrorAndWriteResponse(err.Error(), http.StatusInternalServerError, w)
+			return
+		}
+		if _, err := w.Write(body); err != nil {
+			logErrorAndWriteResponse(err.Error(), http.StatusInternalServerError, w)
+		}
+	}
+}
+
+// DeleteCertificateRequest handler receives an id as a path parameter,
+// deletes the corresponding Certificate Request, and returns a http.StatusNoContent on success
+func DeleteCertificateRequest(env *Environment) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		insertId, err := env.DB.DeleteCSR(id)
+		if err != nil {
+			if errors.Is(err, db.ErrIdNotFound) {
+				logErrorAndWriteResponse(err.Error(), http.StatusNotFound, w)
+				return
+			}
+			logErrorAndWriteResponse(err.Error(), http.StatusInternalServerError, w)
+			return
+		}
+		w.WriteHeader(http.StatusAccepted)
+		if _, err := w.Write([]byte(strconv.FormatInt(insertId, 10))); err != nil {
+			logErrorAndWriteResponse(err.Error(), http.StatusInternalServerError, w)
+		}
+	}
+}
+
+// PostCertificate handler receives an id as a path parameter,
+// and attempts to add a given certificate to the corresponding certificate request
+func PostCertificate(env *Environment) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		cert, err := io.ReadAll(r.Body)
+		if err != nil {
+			logErrorAndWriteResponse(err.Error(), http.StatusBadRequest, w)
+			return
+		}
+		id := r.PathValue("id")
+		insertId, err := env.DB.UpdateCSR(id, string(cert))
+		if err != nil {
+			if errors.Is(err, db.ErrIdNotFound) ||
+				err.Error() == "certificate does not match CSR" ||
+				strings.Contains(err.Error(), "cert validation failed") {
+				logErrorAndWriteResponse(err.Error(), http.StatusBadRequest, w)
+				return
+			}
+			logErrorAndWriteResponse(err.Error(), http.StatusInternalServerError, w)
+			return
+		}
+		insertIdStr := strconv.FormatInt(insertId, 10)
+		if env.SendPebbleNotifications {
+			err := SendPebbleNotification("notary.com/certificate/update", insertIdStr)
+			if err != nil {
+				log.Printf("pebble notify failed: %s. continuing silently.", err.Error())
+			}
+		}
+		w.WriteHeader(http.StatusCreated)
+		if _, err := w.Write([]byte(insertIdStr)); err != nil {
+			logErrorAndWriteResponse(err.Error(), http.StatusInternalServerError, w)
+		}
+	}
+}
+
+func RejectCertificate(env *Environment) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		insertId, err := env.DB.UpdateCSR(id, "rejected")
+		if err != nil {
+			if errors.Is(err, db.ErrIdNotFound) {
+				logErrorAndWriteResponse(err.Error(), http.StatusNotFound, w)
+				return
+			}
+			logErrorAndWriteResponse(err.Error(), http.StatusInternalServerError, w)
+			return
+		}
+		insertIdStr := strconv.FormatInt(insertId, 10)
+		if env.SendPebbleNotifications {
+			err := SendPebbleNotification("notary.com/certificate/update", insertIdStr)
+			if err != nil {
+				log.Printf("pebble notify failed: %s. continuing silently.", err.Error())
+			}
+		}
+		w.WriteHeader(http.StatusAccepted)
+		if _, err := w.Write([]byte(insertIdStr)); err != nil {
+			logErrorAndWriteResponse(err.Error(), http.StatusInternalServerError, w)
+		}
+	}
+}
+
+// DeleteCertificate handler receives an id as a path parameter,
+// and attempts to add a given certificate to the corresponding certificate request
+func DeleteCertificate(env *Environment) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		insertId, err := env.DB.UpdateCSR(id, "")
+		if err != nil {
+			if errors.Is(err, db.ErrIdNotFound) {
+				logErrorAndWriteResponse(err.Error(), http.StatusBadRequest, w)
+				return
+			}
+			logErrorAndWriteResponse(err.Error(), http.StatusInternalServerError, w)
+			return
+		}
+		insertIdStr := strconv.FormatInt(insertId, 10)
+		if env.SendPebbleNotifications {
+			err := SendPebbleNotification("notary.com/certificate/update", insertIdStr)
+			if err != nil {
+				log.Printf("pebble notify failed: %s. continuing silently.", err.Error())
+			}
+		}
+		w.WriteHeader(http.StatusAccepted)
+		if _, err := w.Write([]byte(insertIdStr)); err != nil {
+			logErrorAndWriteResponse(err.Error(), http.StatusInternalServerError, w)
+		}
+	}
+}

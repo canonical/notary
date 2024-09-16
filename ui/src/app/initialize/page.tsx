6e4a9ff32b8109beb4882b3bf64bee31082721e2
guillaume <guillaume.belanger27@gmail.com>
2024-09-16 11:59:59 -0400

    chore: use Canonical react components (initialize page)
    Signed-off-by: guillaume <guillaume.belanger27@gmail.com>

--- ui/src/app/initialize/page.tsx
+++ ui/src/app/initialize/page.tsx
@@ -9,6 +9,7 @@ import { useAuth } from "../auth/authContext"
 import { Logo } from "../nav"
 import { useCookies } from "react-cookie"
 import { statusResponse } from "../types"
+import { Navigation, Theme, Input, PasswordToggle, Button, Form } from "@canonical/react-components";
 
 
 export default function Initialize() {
@@ -50,18 +51,20 @@ export default function Initialize() {
     const [password1, setPassword1] = useState<string>("")
     const [password2, setPassword2] = useState<string>("")
     const passwordsMatch = password1 === password2
+    const password1Error = password1 && !passwordIsValid(password1) ? "Password is not valid" : ""
+    const password2Error = password2 && !passwordsMatch ? "Passwords do not match" : ""
+
     const [errorText, setErrorText] = useState<string>("")
-    const [showPassword1, setShowPassword1] = useState<boolean>(false)
     const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) => { setUsername(event.target.value) }
     const handlePassword1Change = (event: ChangeEvent<HTMLInputElement>) => { setPassword1(event.target.value) }
     const handlePassword2Change = (event: ChangeEvent<HTMLInputElement>) => { setPassword2(event.target.value) }
     return (
         <>
-            <div style={{ backgroundColor: "#262626", height: "6.5vh" }}>
-                <div style={{ marginLeft: "30px" }}>
-                    <Logo />
-                </div>
-            </div>
+            <Navigation
+                items={[]}
+                logo={< Logo />}
+                theme={Theme.DARK}
+            />
             <div style={{
                 display: "flex",
                 alignContent: "center",
@@ -79,60 +82,44 @@ export default function Initialize() {
                             <h2>Initialize Notary</h2>
                         </div>
                         <div className="p-panel__content">
-                            <form className={"p-form-validation " + ((!passwordIsValid(password1) && password1 != "") || (!passwordsMatch && password2 != "") ? "is-error" : "")}>
-                                <div className="p-form__group row">
-                                    <h4>Create the initial admin user</h4>
-                                    <label className="p-form__label">Username</label>
-                                    <input type="text" id="InputUsername" name="InputUsername" onChange={handleUsernameChange} />
-                                    <div>
-                                        <label className="p-form__label">Password</label>
-                                        <button className="p-button--base u-no-margin--bottom has-icon" style={{ float: "right" }} aria-live="polite" aria-controls="password" onClick={(e) => { e.preventDefault(); setShowPassword1(!showPassword1) }}>
-                                            {showPassword1 ? (
-                                                <>
-                                                    <span className="p-form-password-toggle__label">
-                                                        Hide
-                                                    </span>
-                                                    <i className="p-icon--hide"></i>
-                                                </>
-                                            ) : (
-                                                <>
-                                                    <span className="p-form-password-toggle__label">
-                                                        Show
-                                                    </span>
-                                                    <i className="p-icon--show"></i>
-                                                </>
-                                            )}
-                                        </button>
-                                    </div>
-                                    <input className="p-form-validation__input" type={showPassword1 ? "text" : "password"} id="password1" name="password" placeholder="******" autoComplete="current-password" required={true} onChange={handlePassword1Change} />
-                                    <p className="p-form-help-text">
-                                        Password must have 8 or more characters, must include at least one capital letter, one lowercase letter, and either a number or a symbol.
-                                    </p>
-                                    <label htmlFor="p-form__label">Confirm Password</label>
-                                    <input className="p-form-validation__input" type="password" id="InputPassword" name="password2" placeholder="******" autoComplete="current-password" onChange={handlePassword2Change} />
-                                    {!passwordIsValid(password1) && password1 != "" && <p className="p-form-validation__message">Password is not valid</p>}
-                                    {passwordIsValid(password1) && !passwordsMatch && password2 != "" && <p className="p-form-validation__message">Passwords do not match</p>}
-                                    {errorText &&
-                                        <div className="p-notification--negative">
-                                            <div className="p-notification__content">
-                                                <h5 className="p-notification__title">Error</h5>
-                                                <p className="p-notification__message">{errorText.split("error: ")}</p>
-                                            </div>
-                                        </div>
-                                    }
-                                    {!passwordsMatch || !passwordIsValid(password1) ? (
-                                        <>
-                                            <button className="p-button--positive" type="submit" name="submit" disabled={true}>Submit</button>
-                                        </>
-                                    ) : (
-                                        <button className="p-button--positive" type="submit" name="submit" onClick={(event) => { event.preventDefault(); postUserMutation.mutate({ username: username, password: password1 }) }}>Submit</button>
-                                    )}
-                                </div>
-                            </form>
+                            <Form>
+                                <h4>Create the initial admin user</h4>
+                                <Input
+                                    id="InputUsername"
+                                    label="Username"
+                                    type="text"
+                                    onChange={handleUsernameChange}
+                                />
+                                <PasswordToggle
+                                    help="Password must have 8 or more characters, must include at least one capital letter, one lowercase letter, and either a number or a symbol."
+                                    id="password1"
+                                    label="Password"
+                                    onChange={handlePassword1Change}
+                                    error={password1Error}
+                                />
+                                <PasswordToggle
+                                    id="password2"
+                                    label="Confirm Password"
+                                    onChange={handlePassword2Change}
+                                    error={password2Error}
+                                />
+                                <Button
+                                    appearance="positive"
+                                    disabled={!passwordsMatch || !passwordIsValid(password1)}
+                                    onClick={(event) => {
+                                        event.preventDefault();
+                                        if (passwordsMatch && passwordIsValid(password1)) {
+                                            postUserMutation.mutate({ username: username, password: password1 });
+                                        }
+                                    }}
+                                >
+                                    Submit
+                                </Button>
+                            </Form>
                         </div>
                     </fieldset>
                 </div >
-            </div>
+            </div >
         </>
     )
 }

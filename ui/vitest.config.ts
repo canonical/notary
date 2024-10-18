bab8b0b10a35bba630819651c46888f05f4cedba
kayra1 <gemalmazkayra@gmail.com>
2024-10-18 22:28:52 +0300

    fix: remove unneeded test

--- ui/vitest.config.ts
+++ ui/vitest.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vitest/config'
+import { resolve } from 'node:path'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
@@ -7,4 +8,7 @@ export default defineConfig({
         environment: 'jsdom',
         reporters: ['default', 'github-actions']
     },
+    resolve: {
+        alias: [{ find: "@", replacement: resolve(__dirname, "./src") }]
+    }
 })

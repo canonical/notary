ff5e51184c62bc68263e353703a2517301abd61a
yazansalti <yazan.salti@canonical.com>
2025-01-09 18:07:05 +0100

    Test improvements for certificate db

--- internal/db/db_certificates_test.go
+++ internal/db/db_certificates_test.go
@@ -78,17 +78,37 @@ func TestCertificatesEndToEnd(t *testing.T) {
 	if retrievedCSR.Status != "Revoked" {
 		t.Fatalf("Couldn't revoke certificate")
 	}
+	chain, err := database.GetCertificateChain(db.ByCertificatePEM(AppleCert))
+	if err != nil {
+		t.Fatalf("Couldn't get certificate chain: %s", err)
+	}
+	if len(chain) != 3 {
+		t.Fatalf("Expected 3 certificates in the chain, got %d", len(chain))
+	}
+	if chain[0].CertificatePEM != AppleCert ||
+		chain[1].CertificatePEM != IntermediateCert ||
+		chain[2].CertificatePEM != RootCert {
+		t.Fatalf("Certificate chain order or content incorrect")
+	}
+
+	err = database.DeleteCertificate(db.ByCertificatePEM(AppleCert))
+	if err != nil {
+		t.Fatalf("Couldn't delete certificate: %s", err)
+	}
+	_, err = database.GetCertificateChain(db.ByCertificatePEM(AppleCert))
+	if err == nil {
+		t.Fatalf("Expected error when retrieving deleted certificate, got nil")
+	}
 }
 
-func TestCreateCertificateFails(t *testing.T) {
+func TestGetCertificateFails(t *testing.T) {
 	database, _ := db.NewDatabase(":memory:")
 	defer database.Close()
 
-	database.CreateCertificateRequest(AppleCSR)  //nolint:errcheck
-	database.CreateCertificateRequest(BananaCSR) //nolint:errcheck
+	database.CreateCertificateRequest(AppleCSR)
 	err := database.AddCertificateChainToCertificateRequest(db.ByCSRPEM(AppleCSR), AppleCert+IntermediateCert+"some extra string"+RootCert)
 	if err != nil {
-		t.Fatalf("The certificate should have uploaded successfully")
+		t.Fatalf("The certificate should have been uploaded successfully")
 	}
 
 	cert, err := database.GetCertificate(db.ByCertificatePEM(AppleCert))
@@ -96,17 +116,24 @@ func TestCreateCertificateFails(t *testing.T) {
 		t.Fatalf("The certificate that was uploaded does not match the certificate that was given.\n Retrieved: %s\nGiven: %s", cert.CertificatePEM, AppleCert)
 	}
 
+	cert, err = database.GetCertificate(db.ByCertificateID(2))
+	if err != nil || cert.CertificatePEM != AppleCert {
+		t.Fatalf("The certificate that was uploaded does not match the certificate that was given.\n Retrieved: %s\nGiven: %s", cert.CertificatePEM, AppleCert)
+	}
+
 	_, err = database.GetCertificate(db.ByCertificatePEM("nonexistent cert"))
 	if err == nil {
-		t.Fatalf("An error should be returned.")
+		t.Fatalf("An error should be returned when retrieving a nonexistent certificate.")
 	}
+
 	_, err = database.GetCertificate(db.ByCertificatePEM(""))
 	if err == nil {
-		t.Fatalf("An error should be returned.")
+		t.Fatalf("An error should be returned when retrieving a certificate with an empty PEM string.")
 	}
+
 	_, err = database.GetCertificate(db.ByCertificateID(5))
 	if err == nil {
-		t.Fatalf("An error should be returned.")
+		t.Fatalf("An error should be returned when retrieving a certificate with an invalid ID.")
 	}
 	_, err = database.GetCertificate(db.ByCertificateID(0))
 	if err == nil {
@@ -118,32 +145,62 @@ func TestCertificateAddFails(t *testing.T) {
 	database, _ := db.NewDatabase(":memory:")
 	defer database.Close()
 
-	database.CreateCertificateRequest(AppleCSR)  //nolint:errcheck
-	database.CreateCertificateRequest(BananaCSR) //nolint:errcheck
+	database.CreateCertificateRequest(AppleCSR)
+	database.CreateCertificateRequest(BananaCSR)
 	InvalidCert := strings.ReplaceAll(BananaCert, "/", "+")
 	if err := database.AddCertificateChainToCertificateRequest(db.ByCSRPEM(BananaCSR), InvalidCert); err == nil {
-		t.Fatalf("Expected updating with invalid cert to fail")
+		t.Fatalf("Expected adding certificate chain with invalid cert to fail")
 	}
 	if err := database.AddCertificateChainToCertificateRequest(db.ByCSRPEM(AppleCSR), BananaCert); err == nil {
-		t.Fatalf("Expected updating with mismatched cert to fail")
+		t.Fatalf("Expected adding certificate chain with mismatched cert to fail")
 	}
 	if err := database.AddCertificateChainToCertificateRequest(db.ByCSRPEM(AppleCSR), ""); err == nil {
-		t.Fatalf("Expected updating with empty string to fail")
+		t.Fatalf("Expected adding certificate chain with empty string to fail")
 	}
 	if err := database.AddCertificateChainToCertificateRequest(db.ByCSRPEM(AppleCSR), "random string"); err == nil {
-		t.Fatalf("Expected updating with random string to fail")
+		t.Fatalf("Expected adding certificate chain with random string to fail")
 	}
 
 	if err := database.AddCertificateChainToCertificateRequest(db.ByCSRID(1), InvalidCert); err == nil {
-		t.Fatalf("Expected updating with invalid cert to fail")
+		t.Fatalf("Expected adding certificate chain with invalid cert to fail")
 	}
 	if err := database.AddCertificateChainToCertificateRequest(db.ByCSRID(2), BananaCert); err == nil {
-		t.Fatalf("Expected updating with mismatched cert to fail")
+		t.Fatalf("Expected adding certificate chain with mismatched cert to fail")
 	}
 	if err := database.AddCertificateChainToCertificateRequest(db.ByCSRID(2), ""); err == nil {
-		t.Fatalf("Expected updating with empty string to fail")
+		t.Fatalf("Expected adding certificate chain with empty string to fail")
 	}
 	if err := database.AddCertificateChainToCertificateRequest(db.ByCSRID(2), "random string"); err == nil {
-		t.Fatalf("Expected updating with random string to fail")
+		t.Fatalf("Expected adding certificate chain with random string to fail")
+	}
+}
+
+func TestGetCertificateChainFails(t *testing.T) {
+	database, _ := db.NewDatabase(":memory:")
+	defer database.Close()
+
+	database.CreateCertificateRequest(AppleCSR)
+	database.CreateCertificateRequest(BananaCSR)
+	database.AddCertificateChainToCertificateRequest(db.ByCSRPEM(AppleCSR), AppleCert+IntermediateCert+RootCert)
+	database.AddCertificateChainToCertificateRequest(db.ByCSRPEM(BananaCSR), BananaCert+IntermediateCert+RootCert)
+	_, err := database.GetCertificateChain(db.ByCertificatePEM(AppleCert))
+	if err != nil {
+		t.Fatalf("An error should not be returned when retrieving a certificate chain.")
+	}
+	_, err = database.GetCertificateChain(db.ByCertificateID(1))
+	if err != nil {
+		t.Fatalf("An error should not be returned when retrieving a certificate chain.")
+	}
+	_, err = database.GetCertificateChain(db.ByCertificatePEM("nonexistent cert"))
+	if err == nil {
+		t.Fatalf("An error should be returned when retrieving a nonexistent certificate chain.")
+	}
+	_, err = database.GetCertificateChain(db.ByCertificatePEM(""))
+	if err == nil {
+		t.Fatalf("An error should be returned when retrieving a certificate chain with an empty PEM string.")
+	}
+	_, err = database.GetCertificateChain(db.ByCertificateID(5))
+	if err == nil {
+		t.Fatalf("An error should be returned when retrieving a certificate chain with an invalid ID.")
 	}
 }

6aa14495797b66620ba9085bf9cca8e719a902a8
Guillaume Belanger <guillaume.belanger27@gmail.com>
2025-04-23 07:05:39 -0400

    chore: alias logging import in main
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- cmd/notary/main.go
+++ cmd/notary/main.go
@@ -9,7 +9,7 @@ import (
 	"os/signal"
 
 	"github.com/canonical/notary/internal/config"
-	"github.com/canonical/notary/internal/logger"
+	l "github.com/canonical/notary/internal/logger"
 	"github.com/canonical/notary/internal/server"
 )
 
@@ -24,13 +24,13 @@ func main() {
 	if err != nil {
 		log.Fatalf("Couldn't validate config file: %s", err)
 	}
-	l, err := logger.NewLogger(&conf.Logging)
+	logger, err := l.NewLogger(&conf.Logging)
 	if err != nil {
-		l.Fatalf("Couldn't create logger: %s", err)
+		logger.Fatalf("Couldn't create logger: %s", err)
 	}
-	srv, err := server.New(conf.Port, conf.Cert, conf.Key, conf.DBPath, conf.ExternalHostname, conf.PebbleNotificationsEnabled, l)
+	srv, err := server.New(conf.Port, conf.Cert, conf.Key, conf.DBPath, conf.ExternalHostname, conf.PebbleNotificationsEnabled, logger)
 	if err != nil {
-		l.Fatalf("Couldn't create server: %s", err)
+		logger.Fatalf("Couldn't create server: %s", err)
 	}
 
 	idleConnsClosed := make(chan struct{})
@@ -38,17 +38,17 @@ func main() {
 		sigint := make(chan os.Signal, 1)
 		signal.Notify(sigint, os.Interrupt)
 		<-sigint
-		l.Infof("Interrupt signal received")
+		logger.Infof("Interrupt signal received")
 		if err := srv.Shutdown(context.Background()); err != nil {
-			l.Errorf("HTTP server Shutdown error: %v", err)
+			logger.Errorf("HTTP server Shutdown error: %v", err)
 		}
 		close(idleConnsClosed)
 	}()
 
-	l.Infof("Starting server at %s", srv.Addr)
+	logger.Infof("Starting server at %s", srv.Addr)
 	if err := srv.ListenAndServeTLS("", ""); err != http.ErrServerClosed {
-		l.Fatalf("HTTP server ListenAndServe: %v", err)
+		logger.Fatalf("HTTP server ListenAndServe: %v", err)
 	}
-	l.Infof("Shutting down server")
+	logger.Infof("Shutting down server")
 	<-idleConnsClosed
 }

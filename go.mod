31c0edda01c22a508c22cda55c9b8bcaccea9d6c
Kayra <kayra.gemalmaz@canonical.com>
2025-06-30 22:32:12 +0300

    feat: rework config tests

--- go.mod
+++ go.mod
@@ -4,14 +4,13 @@ go 1.24.0
 
 require (
 	github.com/golang-jwt/jwt/v5 v5.2.2
+	github.com/google/go-cmp v0.7.0
 	github.com/mattn/go-sqlite3 v1.14.24
 	github.com/prometheus/client_golang v1.21.1
 	go.uber.org/zap v1.27.0
 	golang.org/x/crypto v0.36.0
 	gopkg.in/yaml.v3 v3.0.1
 )
-
-require go.uber.org/multierr v1.10.0 // indirect
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
@@ -23,6 +22,7 @@ require (
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.62.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 	google.golang.org/protobuf v1.36.1 // indirect
 )
--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -7,12 +7,165 @@ import (
 	"testing"
 
 	"github.com/canonical/notary/internal/config"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestValidConfig(t *testing.T) {
+	mustPrepareConfigFiles(t)
+
+	cases := []struct {
+		desc       string
+		configYAML string
+		wantCfg    config.Config
+	}{
+		{"minimal config", validMinimalConfig, config.Config{
+			Cert:                       []byte(validCert),
+			Key:                        []byte(validPK),
+			ExternalHostname:           "localhost",
+			DBPath:                     "./notary.db",
+			Port:                       8000,
+			PebbleNotificationsEnabled: false,
+			Logging: config.Logging{
+				System: config.SystemLoggingConfig{
+					Level:  "debug",
+					Output: "stdout",
+				},
+			},
+			EncryptionBackend: config.EncryptionBackend{
+				Type:   config.None,
+				PKCS11: nil,
+				Vault:  nil,
+			},
+		}}, // This case tests the expected default values for missing fields are filled correctly
+		{"full config", validFullConfig, config.Config{
+			Cert:                       []byte(validCert),
+			Key:                        []byte(validPK),
+			ExternalHostname:           "example.com",
+			DBPath:                     "./notary.db",
+			Port:                       8000,
+			PebbleNotificationsEnabled: false,
+			Logging: config.Logging{
+				System: config.SystemLoggingConfig{
+					Level:  "info",
+					Output: "some/file",
+				},
+			},
+			EncryptionBackend: config.EncryptionBackend{
+				Type: config.PKCS11,
+				PKCS11: &config.PKCS11BackendConfigYaml{
+					LibPath: "path/to/lib",
+					KeyID:   func() *uint16 { v := uint16(16); return &v }(),
+					Pin:     "0001password",
+				},
+			},
+		}}, // This case tests that the variables from the yaml are correctly copied to the final config
+	}
+	for _, tc := range cases {
+		t.Run(tc.desc, func(t *testing.T) {
+			err := os.WriteFile("config.yaml", []byte(tc.configYAML), 0o644)
+			if err != nil {
+				t.Fatalf("Error writing config file")
+			}
+			gotCfg, err := config.ValidateConfig("config.yaml")
+			if err != nil {
+				t.Fatalf("ValidateConfig(%q) = %v, want nil", "config.yaml", err)
+			}
+			if !cmp.Equal(gotCfg, tc.wantCfg) {
+				t.Fatalf("ValidateConfig returned unexpected diff (-want+got):\n%v", cmp.Diff(tc.wantCfg, gotCfg))
+			}
+		})
+	}
+}
+
+func TestInvalidConfig(t *testing.T) {
+	mustPrepareConfigFiles(t)
+
+	cases := []struct {
+		desc       string
+		configYAML string
+		wantErr    string
+	}{
+		{"no cert path", noCertPathConfig, "`cert_path` is empty"},
+		{"no key path", noKeyPathConfig, "`key_path` is empty"},
+		{"no db path", noDBPathConfig, "`db_path` is empty"},
+		{"wrong cert path", wrongCertPathConfig, "no such file or directory"},
+		{"wrong key path", wrongKeyPathConfig, "no such file or directory"},
+		{"invalid yaml", invalidYAMLConfig, "unmarshal errors"},
+		{"no encryption backend", noEncryptionBackendConfig, "`encryption_backend` config is missing, it must be a map with backends, empty map means no encryption"},
+		{"invalid pkcs11 encryption backend config", invalidEncryptionBackendConfigType, "invalid encryption backend type; must be 'vault' or 'pkcs11'"},
+		{"incomplete pkcs11 encryption backend config", incompleteEncryptionBackendConfig, "pin is missing"},
+	}
+	for _, tc := range cases {
+		t.Run(tc.desc, func(t *testing.T) {
+			err := os.WriteFile("config.yaml", []byte(tc.configYAML), 0o644)
+			if err != nil {
+				t.Errorf("Failed writing config file: %v", err)
+			}
+			_, err = config.ValidateConfig("config.yaml")
+			if err == nil || !strings.Contains(err.Error(), tc.wantErr) {
+				t.Errorf("config.Validate(%v) = %v, want %v", tc.configYAML, err, tc.wantErr)
+			}
+		})
+	}
+}
+
+func mustPrepareConfigFiles(t *testing.T) {
+	t.Helper()
+	testfolder := t.TempDir()
+	err := os.WriteFile(testfolder+"/cert_test.pem", []byte(validCert), 0o644)
+	if err != nil {
+		log.Fatalf("couldn't create temp testing file: %v", err)
+	}
+	err = os.WriteFile(testfolder+"/key_test.pem", []byte(validPK), 0o644)
+	if err != nil {
+		log.Fatalf("couldn't create temp testing file: %v", err)
+	}
+	t.Chdir(testfolder)
+}
+
+const (
+	validCert          = `----- CERTIFICATE -----`
+	validPK            = `----- PRIVATE KEY -----`
+	validMinimalConfig = `
+key_path:  "./key_test.pem"
+cert_path: "./cert_test.pem"
+db_path: "./notary.db"
+port: 8000
+encryption_backend: {}
+`
+	validFullConfig = `
+key_path:  "./key_test.pem"
+cert_path: "./cert_test.pem"
+external_hostname: "example.com"
+db_path: "./notary.db"
+pebble_notifications: false
+port: 8000
+logging:
+ system:
+  level: "info"
+  output: "some/file"
+encryption_backend:
+ yubihsm:
+  pkcs11:
+   lib_path: "path/to/lib"
+   aes_encryption_key_id: 16
+   pin: "0001password"
+`
 )
 
 const (
-	validCert   = `Whatever cert content`
-	validPK     = `Whatever key content`
-	validConfig = `key_path:  "./key_test.pem"
+	noCertPathConfig = `
+key_path:  "./key_test.pem"
+external_hostname: "example.com"
+db_path: "./notary.db"
+port: 8000
+logging:
+  system:
+    level: "debug"
+    output: "stdout"
+encryption_backend: {}
+`
+	noKeyPathConfig = `
 cert_path: "./cert_test.pem"
 external_hostname: "example.com"
 db_path: "./notary.db"
@@ -21,26 +174,10 @@ logging:
   system:
     level: "debug"
     output: "stdout"
-encryption_backend: {}`
-	noCertPathConfig = `key_path:  "./key_test.pem"
-external_hostname: "example.com"
-db_path: "./notary.db"
-port: 8000
-logging:
-  system:
-    level: "debug"
-    output: "stdout"
-encryption_backend: {}`
-	noKeyPathConfig = `cert_path: "./cert_test.pem"
-external_hostname: "example.com"
-db_path: "./notary.db"
-port: 8000
-logging:
-  system:
-    level: "debug"
-    output: "stdout"
-encryption_backend: {}`
-	noExternalHostnameConfig = `key_path:  "./key_test.pem"
+encryption_backend: {}
+`
+	noExternalHostnameConfig = `
+key_path:  "./key_test.pem"
 cert_path: "./cert_test.pem"
 db_path: "./notary.db"
 port: 8000
@@ -48,8 +185,10 @@ logging:
   system:
     level: "debug"
     output: "stdout"
-encryption_backend: {}`
-	noDBPathConfig = `key_path:  "./key_test.pem"
+encryption_backend: {}
+`
+	noDBPathConfig = `
+key_path:  "./key_test.pem"
 external_hostname: "example.com"
 cert_path: "./cert_test.pem"
 port: 8000
@@ -57,8 +196,10 @@ logging:
   system:
     level: "debug"
     output: "stdout"
-encryption_backend: {}`
-	wrongCertPathConfig = `key_path:  "./key_test.pem"
+encryption_backend: {}
+`
+	wrongCertPathConfig = `
+key_path:  "./key_test.pem"
 cert_path: "./cert_test_wrong.pem"
 external_hostname: "example.com"
 db_path: "./notary.db"
@@ -66,8 +207,11 @@ port: 8000
 logging:
   system:
     level: "debug"
-    output: "stdout"`
-	wrongKeyPathConfig = `key_path:  "./key_test_wrong.pem"
+    output: "stdout"
+encryption_backend: {}
+`
+	wrongKeyPathConfig = `
+key_path:  "./key_test_wrong.pem"
 cert_path: "./cert_test.pem"
 external_hostname: "example.com"
 db_path: "./notary.db"
@@ -76,14 +220,10 @@ logging:
   system:
     level: "debug"
     output: "stdout"
-encryption_backend: {}`
-	noLoggingConfig = `key_path:  "./key_test.pem"
-cert_path: "./cert_test.pem"
-external_hostname: "example.com"
-db_path: "./notary.db"
-port: 8000
-encryption_backend: {}`
-	invalidEncryptionBackendConfigType = `key_path:  "./key_test.pem"
+encryption_backend: {}
+`
+	invalidEncryptionBackendConfigType = `
+key_path:  "./key_test.pem"
 cert_path: "./cert_test.pem"
 external_hostname: "example.com"
 db_path: "./notary.db"
@@ -97,9 +237,10 @@ encryption_backend:
     invalid:
       lib_path: "/usr/local/lib/pkcs11/yubihsm_pkcs11.so"
       aes_encryption_key_id: 0x1234
-      pin: "0001password"`
-
-	noEncryptionBackendConfig = `key_path:  "./key_test.pem"
+      pin: "0001password"
+`
+	noEncryptionBackendConfig = `
+key_path:  "./key_test.pem"
 cert_path: "./cert_test.pem"
 external_hostname: "example.com"
 db_path: "./notary.db"
@@ -109,7 +250,8 @@ logging:
     level: "debug"
     output: "stdout"
 `
-	incompleteEncryptionBackendConfig = `key_path:  "./key_test.pem"
+	incompleteEncryptionBackendConfig = `
+key_path:  "./key_test.pem"
 cert_path: "./cert_test.pem"
 external_hostname: "example.com"
 db_path: "./notary.db"
@@ -127,105 +269,3 @@ encryption_backend:
 	invalidYAMLConfig = `just_an=invalid
 yaml.here`
 )
-
-func TestMain(m *testing.M) {
-	testfolder, err := os.MkdirTemp("./", "configtest-")
-	if err != nil {
-		log.Fatalf("couldn't create temp directory: %s", err)
-	}
-	err = os.WriteFile(testfolder+"/cert_test.pem", []byte(validCert), 0o644)
-	if err != nil {
-		log.Fatalf("couldn't create temp testing file: %s", err)
-	}
-	err = os.WriteFile(testfolder+"/key_test.pem", []byte(validPK), 0o644)
-	if err != nil {
-		log.Fatalf("couldn't create temp testing file: %s", err)
-	}
-	if err := os.Chdir(testfolder); err != nil {
-		log.Fatalf("couldn't enter testing directory: %s", err)
-	}
-
-	exitval := m.Run()
-
-	if err := os.Chdir("../"); err != nil {
-		log.Fatalf("couldn't change back to parent directory: %s", err)
-	}
-	if err := os.RemoveAll(testfolder); err != nil {
-		log.Fatalf("couldn't remove temp testing directory: %s", err)
-	}
-	os.Exit(exitval)
-}
-
-func TestGoodConfigSuccess(t *testing.T) {
-	writeConfigErr := os.WriteFile("config.yaml", []byte(validConfig), 0o644)
-	if writeConfigErr != nil {
-		t.Fatalf("Error writing config file")
-	}
-	conf, err := config.Validate("config.yaml")
-	if err != nil {
-		t.Fatalf("Error occurred: %s", err)
-	}
-	if conf.Cert == nil {
-		t.Fatalf("No certificates were configured for server")
-	}
-	if conf.Key == nil {
-		t.Fatalf("No key was configured for server")
-	}
-	if conf.ExternalHostname != "example.com" {
-		t.Fatalf("External hostname was not configured correctly")
-	}
-	if conf.DBPath == "" {
-		t.Fatalf("No database path was configured for server")
-	}
-	if conf.Port != 8000 {
-		t.Fatalf("Port was not configured correctly")
-	}
-}
-
-func TestBadConfigFail(t *testing.T) {
-	cases := []struct {
-		Name          string
-		ConfigYAML    string
-		ExpectedError string
-	}{
-		{"no cert path", noCertPathConfig, "`cert_path` is empty"},
-		{"no key path", noKeyPathConfig, "`key_path` is empty"},
-		{"no db path", noDBPathConfig, "`db_path` is empty"},
-		{"wrong cert path", wrongCertPathConfig, "no such file or directory"},
-		{"wrong key path", wrongKeyPathConfig, "no such file or directory"},
-		{"invalid yaml", invalidYAMLConfig, "unmarshal errors"},
-		{"no logging", noLoggingConfig, "`logging` is empty"},
-		{"No encryption backend", noEncryptionBackendConfig, "`encryption_backend` config is missing, it must be a map with backends, empty map means no encryption"},
-		{"Invalid encryption backend config", invalidEncryptionBackendConfigType, "invalid encryption backend type; must be 'vault' or 'pkcs11'"},
-		{"Incomplete encryption backend config", incompleteEncryptionBackendConfig, "pin is missing"},
-	}
-	for _, tc := range cases {
-		t.Run(tc.Name, func(t *testing.T) {
-			err := os.WriteFile("config.yaml", []byte(tc.ConfigYAML), 0o644)
-			if err != nil {
-				t.Errorf("Failed writing config file: %v", err)
-			}
-			_, err = config.Validate("config.yaml")
-			if err == nil {
-				t.Errorf("Expected error, got nil")
-			}
-			if !strings.Contains(err.Error(), tc.ExpectedError) {
-				t.Errorf("Expected error not found: %s", err)
-			}
-		})
-	}
-}
-
-func TestNoExternalHostname(t *testing.T) {
-	writeConfigErr := os.WriteFile("config.yaml", []byte(noExternalHostnameConfig), 0o644)
-	if writeConfigErr != nil {
-		t.Fatalf("Error writing config file")
-	}
-	conf, err := config.Validate("config.yaml")
-	if err != nil {
-		t.Fatalf("Error occurred: %s", err)
-	}
-	if conf.ExternalHostname != "localhost" {
-		t.Fatalf("External hostname default value wasn't localhost")
-	}
-}

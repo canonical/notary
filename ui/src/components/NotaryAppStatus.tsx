ac4198b95f6c160f96b5a27660e4e80ae7d97951
kayra1 <kayra.gemalmaz@canonical.com>
2026-04-30 16:44:24 +0300

    add small left padding to the version

--- ui/src/components/NotaryAppStatus.tsx
+++ ui/src/components/NotaryAppStatus.tsx
@@ -3,7 +3,10 @@ import { AppStatus } from "@canonical/react-components";
 export default function NotaryAppStatus() {
   return (
     <AppStatus>
-      <span className="p-text--small u-no-margin--bottom">
+      <span
+        className="p-text--small u-no-margin--bottom"
+        style={{ paddingLeft: "10px" }}
+      >
         Version {process.env.VERSION}
       </span>
     </AppStatus>
